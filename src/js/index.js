ff455aee9f936da20d65faf7d0313ff4b04e4b2d
guoxm <guoxm@infcn.com.cn>
2016-11-01 19:53:41 +0800

    页面调整

--- src/js/index.js
+++ src/js/index.js
@@ -26,3 +26,36 @@ $(".wenzhang-ul").mousemove(function(e){
     });
 
 });
+$('.btn-qiehuan').click(function(){
+    if(!($(this).hasClass('click-color'))){
+        $(this).addClass('click-color')
+        $(this).parent().siblings().children().removeClass('click-color');
+    }
+});
+layui.use(['laypage', 'layer'], function() {
+    var laypage = layui.laypage
+    var  layer = layui.layer;
+    laypage({
+        cont: 'page'
+        , pages: 100
+        , skin: '#0077dd'
+    })
+})
+/*
+layui.use('flow', function() {
+    var flow = layui.flow;
+    flow.load({
+        elem: '#LAY_demo' //流加载容器
+        ,scrollElem: '#LAY_demo' //滚动条所在元素，一般不用填，此处只是演示需要。
+        ,done: function(page, next){ //加载下一页
+            //模拟插入
+            setTimeout(function(){
+                var lis = [];
+                for(var i = 0; i < 8; i++){
+                    lis.push('<li>'+ ( (page-1)*8 + i + 1 ) +'</li>')
+                }
+                next(lis.join(''), page > 10 ? 0 : 8);
+            }, 500);
+        }
+    });
+})*/
