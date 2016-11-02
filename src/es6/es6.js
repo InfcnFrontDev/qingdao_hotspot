b2bfa64d1baeca96313f76b13e3fb0306c5d9e91
guoxm <guoxm@infcn.com.cn>
2016-11-02 18:51:01 +0800

    Merge branch 'dev' of http://192.168.10.158/InfcnFrontDev/qingdao_hotspot into dev
    # Conflicts:
    #	src/jade/index.jade
    #	src/js/index.js
    #	src/sass/common.scss

---
+++ src/es6/es6.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+
+Vue.config.debug = Config.debug;            // 只有开发版本可以使用调试模式。
+Vue.config.devtools = Config.devtools;      // 配置是否允许 vue-devtools 检查代码
+Vue.config.silent = Config.silent;          // 取消 Vue.js 所有的日志与警告。
+
+
+new Vue({
+    el: 'body',
+    data(){
+        return {
+            message: 'hello vuejs!'
+        }
+    },
+    ready(){
+    }
+});
