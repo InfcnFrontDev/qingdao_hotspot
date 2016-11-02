df99d32a86b6ab3e32fb5ef84fc600e8bccc154c
wangp <wangp@infcn.com.cn>
2016-11-02 09:59:07 +0800

    浮动框

--- src/js/index.js
+++ src/js/index.js
@@ -15,11 +15,12 @@ layui.use('form', function(){
     //各种基于事件的操作，下面会有进一步介绍
 });
 $(".wenzhang-ul").mousemove(function(e){
-    $(".wenzhang-ul").mouseover(function(){
-        $(".floatbox").show();
+    $(".wenzhang-ul li").mouseover(function(){
+        $(this).find(".floatbox").show();
+        $(this).siblings().find(".floatbox").hide();
     });
-    var $fWidth=$(".floatbox").width()*1/2;
-    $(".floatbox").css({left:e.pageX+10-$fWidth,top:e.pageY+10});
+    /*var $fWidth=$(".floatbox").width()*1/2;
+    $(".floatbox").css({left:e.pageX+10-$fWidth,top:e.pageY+10});*/
 
     $(".wenzhang-ul").mouseout(function(){
         $(".floatbox").hide();
