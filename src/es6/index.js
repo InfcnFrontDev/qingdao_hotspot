ca0338bfacf34b2f6d30536fee70e42aee038cfe
yangkk <yangkk@infcn.com.cn>
2016-11-03 17:08:13 +0800

    ok

--- src/es6/index.js
+++ src/es6/index.js
@@ -75,7 +75,7 @@ new Vue({
     methods: {
         // 选择最n天内，更新时间段
         updateDate(val){
-            if (val != '') {
+            if (val != '-') {
                 // 初始化时间区间，默认30天内
                 let date1, date2 = new Date();
                 date1 = Tools.dateAdd(date2, -(val * 24 * 60 * 60));
@@ -95,6 +95,7 @@ new Vue({
             this.nodata = false;
             TopicApi.topic(this.startDate, this.endDate, this.size, this.sortByFreq).then(function (result) {
                 $this.loading=false;
+                $('.option-content').removeClass('hidden');
 
                 if (result.obj) {
                     $this.topicData = result.obj;
