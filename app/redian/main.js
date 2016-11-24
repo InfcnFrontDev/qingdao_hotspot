15a301b723c1a6b5a22f79e47365719c15c4021b
wangp <wangp@infcn.com.cn>
2016-11-24 20:13:57 +0800

    Merge branch 'dev' of http://192.168.10.158/InfcnFrontDev/qingdao_hotspot into dev
    # Conflicts:
    #	app/js/TopicApi.js
    #	app/js/index.js

--- app/redian/main.js
+++ app/redian/main.js
@@ -4,8 +4,8 @@ $('#redian1').find('.topn').children().change(function(){
 });
 
 update('hotWord','redian1');
-$('#redian1').find('select').val(reSelectval)
-$this.setSize(reSelectval,'hotWord','redian1');
+/*$('#redian1').find('select').val(reSelectval)*/
+/*$this.setSize(reSelectval,'hotWord','redian1');*/
 wenZhangShow();
 
 
