dfab369332c8fd77fcc4c157fa2cc1b79f53f3f5
yangkk <yangkk@infcn.com.cn>
2016-11-25 19:13:17 +0800

    主题

--- app/js/index.js
+++ app/js/index.js
@@ -419,25 +419,10 @@ var fenyeTag = function (qishi, size, tag) {
                 "</div>"
         }
 
-        function parseISO8601(dateStringInRange) {
-            var isoExp = /^\s*(\d{4})-(\d\d)-(\d\d)\s*$/,
-                date = new Date(NaN), month,
-                parts = isoExp.exec(dateStringInRange);
 
-            if(parts) {
-                month = +parts[2];
-                date.setFullYear(parts[1], month - 1, parts[3]);
-                if(month != date.getMonth() + 1) {
-                    date.setTime(NaN);
-                }
-            }
-            return date;
-        }
-
-
-        console.log(arr[0]);
-        console.log(arr[0]._source.question_time);
-        console.log(new Date(arr[0]._source.question_time));
+        // console.log(arr[0]);
+        // console.log(arr[0]._source.question_time);
+        // console.log(new Date(arr[0]._source.question_time));
 
 
 
